5fba1e138f1dc7ff455b06a9b710a206807a879c
Brent <bduhbya@users.noreply.github.com>
2023-07-08 21:28:22 -0700

    Integrate tracing class (#1)
    * Add tracing to dire role
    
    Replace hardcoded log levels and tracing class
    
    * Minor updates
    
    * Add reality and Tracing test
    
    Add reality environment Metadata
    
    Start implementation of Tracing tests
    
    * Complete Tracing unit tests
    
    Add remaining Tracing unit tests for
    Info
    Warning
    Error
    Log levels
    
    * Remove unused code from Tracing tests
    
    * Integrate Tracing mock
    
    Integrate Tracing mock into utility test
    
    Format files
    
    Update dependency files for pytest_mock
    
    * Complete Tracing unit testing
    
    Complete Tracing class unit testing input variations
    
    * Refactor utility tests
    
    Refactor utility tests to:
    1. Remove duplicated code
    2. Add assert messages

--- Tracing.py
+++ Tracing.py
@@ -1,4 +1,3 @@
-import inspect
 from loguru import logger
 
 LOG_LEVEL_DEBUG = "DEBUG"
@@ -6,33 +5,48 @@ LOG_LEVEL_INFO = "INFO"
 LOG_LEVEL_WARNING = "WARNING"
 LOG_LEVEL_ERROR = "ERROR"
 
+LOG_LEVELS = [
+  LOG_LEVEL_DEBUG, LOG_LEVEL_ERROR, LOG_LEVEL_INFO, LOG_LEVEL_WARNING
+]
+
+
 class LogTrace:
-    def __init__(self, logger_name, log_file_path, log_level):
-        logger.add(
-            log_file_path,
-            level=log_level,
-            format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}:{line} | {message}",
-            rotation="10 MB",
-            filter=lambda record: record["extra"].get("logger_name") == logger_name,
-        )
-        self.logger = logger.bind(logger_name=logger_name)
 
-    def log(self, level, message, **kwargs):
-        self.logger.opt(depth=2, exception=None, lazy=False).log(level, message, **kwargs)
+  def __init__(self, logger_name, log_file_path, log_level):
+    if log_file_path is None:
+      raise RuntimeError('log_file_path must be defined')
 
-    def error(self, message, **kwargs):
-        self.log("ERROR", message, **kwargs)
+    if log_level not in LOG_LEVELS:
+      raise RuntimeError(f'log_level must be in {LOG_LEVELS}')
 
-    def warning(self, message, **kwargs):
-        self.log("WARNING", message, **kwargs)
+    logger.add(
+      log_file_path,
+      level=log_level,
+      format=
+      "{time:YYYY-MM-DD HH:mm:ss} | {level} | {module}.{function}:{line} | {message}",
+      rotation="10 MB",
+      filter=lambda record: record["extra"].get("logger_name") == logger_name,
+    )
+    self.logger = logger.bind(logger_name=logger_name)
 
-    def info(self, message, **kwargs):
-        self.log("INFO", message, **kwargs)
+  def log(self, level, message, **kwargs):
+    self.logger.opt(depth=2, exception=None,
+                    lazy=False).log(level, message, **kwargs)
 
-    def debug(self, message, **kwargs):
-        self.log("DEBUG", message, **kwargs)
+  def error(self, message, **kwargs):
+    self.log(LOG_LEVEL_ERROR, message, **kwargs)
+
+  def warning(self, message, **kwargs):
+    self.log(LOG_LEVEL_WARNING, message, **kwargs)
+
+  def info(self, message, **kwargs):
+    self.log(LOG_LEVEL_INFO, message, **kwargs)
+
+  def debug(self, message, **kwargs):
+    self.log(LOG_LEVEL_DEBUG, message, **kwargs)
+
 
 #Quick local test for manual checking
 if __name__ == "__main__":
-    tracer = LogTrace('test_tracer', "Local_test_file.txt", LOG_LEVEL_DEBUG)
-    tracer.error("An error occurred. {error_code}", error_code=123)
+  tracer = LogTrace('test_tracer', "Local_test_file.txt", LOG_LEVEL_DEBUG)
+  tracer.error("An error occurred. {error_code}", error_code=123)
--- Tracing_test.py
+++ Tracing_test.py
@@ -1,34 +1,164 @@
 import pytest
-from Tracing import LogTrace, LOG_LEVEL_DEBUG
+from Tracing import LogTrace, LOG_LEVEL_DEBUG, LOG_LEVEL_INFO, LOG_LEVEL_WARNING, LOG_LEVEL_ERROR
 import os
+from pathlib import Path
 
-TEST_LOG_FILE = "test_log_file.txt"
+TEST_LOG_FILE = 'test_log_file.txt'
+TEST_LOGGER = 'test_logger.txt'
+
+ERROR = 0
+WARNING = 1
+INFO = 2
+DEBUG = 3
+LOG_TEST_DATA = [{
+  'LOG_LINE': 'error log trace',
+  'LINE_TYPE': '| ERROR |'
+}, {
+  'LOG_LINE': 'warning log trace',
+  'LINE_TYPE': '| WARNING |'
+}, {
+  'LOG_LINE': 'info log trace',
+  'LINE_TYPE': '| INFO |'
+}, {
+  'LOG_LINE': 'debug log trace',
+  'LINE_TYPE': '| DEBUG |'
+}]
+
 
 def get_file_content():
-    try:
-        with open(TEST_LOG_FILE, "r") as file:
-            content = file.read()
-            return content
-    except Exception as e:
-      print(f"An error occurred: {e}")
+  try:
+    with open(TEST_LOG_FILE, "r") as file:
+      content = file.read()
+      return content
+  except Exception as e:
+    print(f"An error occurred: {e}")
+
 
 def log_contains(value, logContent):
-    return value in logContent
+  return value in logContent
+
+
+def count_lines(file_path):
+  with open(file_path, 'r') as file:
+    line_count = sum(1 for line in file)
+  return line_count
+
+
+def log_and_verify(traceFun, type, shouldLog):
+  line = LOG_TEST_DATA[type].get('LOG_LINE')
+  lineType = LOG_TEST_DATA[type].get('LINE_TYPE')
+  traceFun(line)
+  content = get_file_content()
+  assert log_contains(
+    line, content) == shouldLog, f"Expected {line} in log to be {shouldLog}"
+  assert log_contains(
+    lineType,
+    content) == shouldLog, f"Expected {lineType} in log to be {shouldLog}"
+
+
+def check_line_count(logFile, count):
+  actual = count_lines(logFile)
+  assert actual == count, f"Expected {count} lines in {logFile}, found {actual}"
+
+
+def verify_assert_and_no_tracing(expectedAssert, testLogger, logFile, level):
+  tracer = None
+  with pytest.raises(expectedAssert):
+    tracer = LogTrace(testLogger, logFile, level)
+
+  file_path = Path(TEST_LOG_FILE)
+  assert tracer == None, "Expected tracer to be None"
+  assert file_path.exists(
+  ) == False, f"Expected file {TEST_LOG_FILE} to not exist"
+
 
 @pytest.fixture(autouse=True)
 def tracing_test_setup():
-    # Code to be executed before each test
-    # Perform any necessary setup or initialization here
-    try:
-        os.remove(TEST_LOG_FILE)
-        print("tracing_test_setup, Removed previous test file: " + TEST_LOG_FILE)
-    except OSError as e:
-        print(f"tracing_test_setup, Error deleting the file: {e}")
+  # Code to be executed before each test
+  try:
+    os.remove(TEST_LOG_FILE)
+    print("tracing_test_setup, Removed previous test file: " + TEST_LOG_FILE)
+  except OSError as e:
+    print(f"tracing_test_setup, Error deleting the file: {e}")
+
+
+def test_debug_logging():
+  tracer = LogTrace(TEST_LOGGER, TEST_LOG_FILE, LOG_LEVEL_DEBUG)
+  log_and_verify(tracer.debug, DEBUG, True)
+  check_line_count(TEST_LOG_FILE, 1)
+  log_and_verify(tracer.info, INFO, True)
+  check_line_count(TEST_LOG_FILE, 2)
+  log_and_verify(tracer.warning, WARNING, True)
+  check_line_count(TEST_LOG_FILE, 3)
+  log_and_verify(tracer.error, ERROR, True)
+  check_line_count(TEST_LOG_FILE, 4)
+
+
+def test_info_logging():
+  tracer = LogTrace(TEST_LOGGER, TEST_LOG_FILE, LOG_LEVEL_INFO)
+  log_and_verify(tracer.debug, DEBUG, False)
+  check_line_count(TEST_LOG_FILE, 0)
+  log_and_verify(tracer.info, INFO, True)
+  check_line_count(TEST_LOG_FILE, 1)
+  log_and_verify(tracer.warning, WARNING, True)
+  check_line_count(TEST_LOG_FILE, 2)
+  log_and_verify(tracer.error, ERROR, True)
+  check_line_count(TEST_LOG_FILE, 3)
+
+
+def test_warn_logging():
+  tracer = LogTrace(TEST_LOGGER, TEST_LOG_FILE, LOG_LEVEL_WARNING)
+  log_and_verify(tracer.debug, DEBUG, False)
+  check_line_count(TEST_LOG_FILE, 0)
+  log_and_verify(tracer.info, INFO, False)
+  check_line_count(TEST_LOG_FILE, 0)
+  log_and_verify(tracer.warning, WARNING, True)
+  check_line_count(TEST_LOG_FILE, 1)
+  log_and_verify(tracer.error, ERROR, True)
+  check_line_count(TEST_LOG_FILE, 2)
 
 
 def test_error_logging():
-    tracer = LogTrace('test_tracer', TEST_LOG_FILE, LOG_LEVEL_DEBUG)
-    tracer.error('An error occurred.', error_code=123)
-    content = get_file_content()
-    assert log_contains('An error occurred.', content)
-    # assert len(logs) == 1
+  tracer = LogTrace(TEST_LOGGER, TEST_LOG_FILE, LOG_LEVEL_ERROR)
+  log_and_verify(tracer.debug, DEBUG, False)
+  check_line_count(TEST_LOG_FILE, 0)
+  log_and_verify(tracer.info, INFO, False)
+  check_line_count(TEST_LOG_FILE, 0)
+  log_and_verify(tracer.warning, WARNING, False)
+  check_line_count(TEST_LOG_FILE, 0)
+  log_and_verify(tracer.error, ERROR, True)
+  check_line_count(TEST_LOG_FILE, 1)
+
+
+def test_empty_logger_name():
+  tracer = LogTrace('', TEST_LOG_FILE, LOG_LEVEL_ERROR)
+  file_path = Path(TEST_LOG_FILE)
+  assert file_path.exists() == True, f"Expected file {TEST_LOG_FILE} to exist"
+
+
+def test_none_logger_name():
+  tracer = LogTrace(None, TEST_LOG_FILE, LOG_LEVEL_ERROR)
+  file_path = Path(TEST_LOG_FILE)
+  assert file_path.exists() == True, f"Expected file {TEST_LOG_FILE} to exist"
+
+
+def test_empty_file_name():
+  verify_assert_and_no_tracing(IsADirectoryError, TEST_LOGGER, '',
+                               LOG_LEVEL_ERROR)
+
+
+def test_none_file_name():
+  verify_assert_and_no_tracing(RuntimeError, TEST_LOGGER, None,
+                               LOG_LEVEL_ERROR)
+
+
+def test_bad_type_log_level():
+  verify_assert_and_no_tracing(RuntimeError, TEST_LOGGER, TEST_LOG_FILE, 5)
+
+
+def test_bad_value_log_level():
+  verify_assert_and_no_tracing(RuntimeError, TEST_LOGGER, TEST_LOG_FILE, "5")
+
+
+def test_none_log_level():
+  verify_assert_and_no_tracing(RuntimeError, TEST_LOGGER, TEST_LOG_FILE, None)
